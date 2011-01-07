df729b8d0ed281ae705a94a57ee84d03bc3eb255
Jay Muenzner <jmuenzner@gmail.com>
2011-01-07 21:51:54 +1100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vespene::Application.config.secret_token = '77e0493bdb72732dfef5da54f063bf9168c9f108cd76894770587f8e137a5d098295921bd1d282dea922ebe3cfec1368b7a4846d91484c93edeb33fd5279adae'
